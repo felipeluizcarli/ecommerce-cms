11f5ec1e189c3748be637c92ff614a2f6e7829fb
FELIPE <felipe_carli@hotmail.com>
2025-10-27 18:27:02 -0300

    AulaDia22102025

--- ecommerce-cms/src/cases/categories/hooks/use-category.ts
+++ ecommerce-cms/src/cases/categories/hooks/use-category.ts
@@ -1,36 +1,37 @@
-import { CategoryService } from "../services/category.service";
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { CategoryDTO } from "../dtos/category.dto";
+import { CategoryService } from "../services/category.service";
+import type { CategoryDTO } from "../dtos/category.dto";
 
-export function useCategory () {
-  return useQuery<CategoryDTO[]>({
-    queryKey: ["categories"],
-    queryFn: CategoryService.list
-  });
+
+export function useCategories() {
+    return useQuery<CategoryDTO[]>({
+        queryKey: ['categories'],
+        queryFn: CategoryService.list
+    });
 }
 
-export function useCategoryById (id: string) {
-  return useQuery<CategoryDTO>({
-    queryKey: ["category", id],
-    queryFn: () => CategoryService.getByid(id),
-    enabled: !!id //-> or Boolean(id)
-  });
+export function useCategory(id: string) {
+    return useQuery<CategoryDTO>({
+        queryKey: ['category', id],
+        queryFn: () => CategoryService.getById(id),
+        enabled: !!id //-> or Boolean(id)
+    });
 }
 
-export function useCreateCategory () {
-  return useMutation<CategoryDTO, Error, Omit<CategoryDTO, 'id'>>({
-    mutationFn: (category, Omit<CategoryDTO, 'id'>) => CategoryService.createca(CategoryDTO)
-  })
+export function useCreateCategory(){
+    return useMutation<CategoryDTO, Error, Omit<CategoryDTO, 'id'>>({
+        mutationFn: (category: Omit<CategoryDTO, 'id'>) => CategoryService.create(category)
+    });
 }
 
-export function useUpdateCategory () {
-  return useMutation<CategoryDTO, Error, {id: string, category: CategoryDTO, 'id'>}>({
-    mutationFn: ({id, category}) => CategoryService.update(id, category)
-  });
+export function useUpdateCategory(){
+    return useMutation<CategoryDTO, Error, {id: string, category: CategoryDTO}>({
+        mutationFn: ({id, category}) => CategoryService.update(id, category)
+    });
 }
 
-export function useUpdateCategory () {
-  return useMutation<void, Error, string>({
-    mutationFn: (id: string) => CategoryService.delete(id)
-  })
+export function useDeleteCategory(){
+    return useMutation<void, Error, string>({
+        mutationFn: (id: string) => CategoryService.delete(id)
+    });
 }
