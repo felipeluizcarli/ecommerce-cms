11ff0e85e0d7e52d83ff90b3a4d41eb1e0dcd191
FELIPE <felipe_carli@hotmail.com>
2025-11-03 18:32:16 -0300

    AulaDia

---
+++ ecommerce-cms/src/App.tsx
@@ -0,0 +1,26 @@
+import { Routes,Route } from "react-router-dom"
+import { CategoryLayout } from "./cases/categories/components/category-layout"
+import { CategoryForm } from "./cases/categories/components/category-form"
+
+
+function App() {
+
+  return (
+    <div className="wrapper">
+      
+
+    <main>
+      <Routes>
+        <Route path="/categories" element={ <CategoryLayout />}>
+          <Route path="new" element={ <CategoryForm />} />
+          <Route path=":id" element={ <CategoryForm />} />
+        </Route>
+        
+      </Routes>
+    </main>
+
+    </div>
+  )
+}
+
+export default App
